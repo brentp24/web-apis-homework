8d19c9827231696b7a94132ec8c580575fcce6d6
Brent Peterson <brentp24@gmail.com>
2020-03-24 17:27:48 -0600

    logic to go from question to question

--- quiz.js
+++ quiz.js
@@ -10,7 +10,7 @@
 // WHEN I answer a question
 // THEN I am presented with another question
 // WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
+// THEN time is subtracted from the clock   X
 // WHEN all questions are answered or the timer reaches 0
 // THEN the game is over
 // WHEN the game is over
@@ -24,6 +24,7 @@ var statusSpan = document.querySelector("#status");
 var minutesDisplay = document.querySelector("#minutes");
 var secondsDisplay = document.querySelector("#seconds");
 var startButton = document.querySelector("#start-button");
+var instructions = document.querySelector("#instructions");
 var totalSeconds = 0;
 var status;
 var secondsElapsed = 0;
@@ -39,6 +40,8 @@ startButton.addEventListener("click", startTimer);
 function startTimer() {
     setTime();
     status = "Running";
+    startButton.style.display = "none";
+    instructions.style.display = "none";
     statusSpan.textContent = status;
     /* the "interval" variable here using "setInterval()" begins the recurring increment of the 
        secondsElapsed variable which is used to check if the time is up */
@@ -47,10 +50,11 @@ function startTimer() {
         //So renderTime() is called here once every second.
         renderTime();
     }, 1000);
-} 
+    showQuestions();
+}
 // set timer to 2 minutes
 function setTime() {
-    var minutes = 2;
+    var minutes = 1;
     clearInterval(interval);
     totalSeconds = minutes * 60;
 }
@@ -91,11 +95,11 @@ function renderTime() {
     secondsDisplay.textContent = getFormattedSeconds();
     // ..and then checks to see if the time has run out
     if (secondsElapsed >= totalSeconds) {
-        alert("Times up!");
+        console.log("Times up!");
         stopTimer();
+        endGame();
     }
 }
-
 /* This function stops the interval and also resets secondsElapsed 
    and calls "setTime()" which effectively reset the timer 
    to the input selections workMinutesInput.value and restMinutesInput.value */
@@ -104,10 +108,50 @@ function stopTimer() {
     setTime();
     renderTime();
 }
-
 //This is where the app is really kicked-off, setTime and renderTime are the two main routines.
 setTime();
 renderTime();
 
 
+questions = ["hi", "hello", 3, 4, 5, 6, 7, 8]
+var counter = 0;
+var questionSpan = document.querySelector("#question");
+var next = document.querySelector(".next");
 
+function showQuestions() {
+    questionSpan.innerHTML = questions[counter];
+}
+
+
+
+next.onclick = function () {
+    if (counter < questions.length - 1) {
+        counter = counter + 1;
+        questionSpan.innerHTML = questions[counter];
+        console.log(questions[counter]);
+
+    } else {console.log("hi");
+    endGame()}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function endGame() {
+    status = "Game over";
+    statusSpan.textContent = status;
+
+    //window.location.href = "alldone.html" ; 
+}
